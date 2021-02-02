212d26a7bc501947b0ea142403e0fade37876804
worker <pestekhin70@yandex.ru>
2021-02-02 17:33:26 +0700

    read & write list json

--- src/main/java/com/tunix70/javaio/Runner.java
+++ src/main/java/com/tunix70/javaio/Runner.java
@@ -1,11 +1,24 @@
 package com.tunix70.javaio;
 
+import com.tunix70.javaio.model.Region;
 import com.tunix70.javaio.repository.io.Util;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Runner {
     public static void main(String[] args) throws IOException {
+//        Util.readFile("src/main/resources/files/regions.json");
+//        Region region = new Region(2l, "EU");
+//        Region region1 = new Region(3l, "FR");
+//
+//        List<Region> regList = new ArrayList<>();
+//        regList.add(region1);
+//        regList.add(region);
+//
+//
+//        Util.writeFile(regList, "src/main/resources/files/regions.json");
         Util.readFile("src/main/resources/files/regions.json");
     }
 }
--- src/main/java/com/tunix70/javaio/repository/io/Util.java
+++ src/main/java/com/tunix70/javaio/repository/io/Util.java
@@ -1,24 +1,44 @@
 package com.tunix70.javaio.repository.io;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.tunix70.javaio.model.Region;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
+
+import java.io.*;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class Util {
-    public static void readFile(String file) throws IOException {
-        try(Reader reader = new FileReader(file)) {
-            Gson gson = new Gson();
+    private static final Gson gson = new Gson();
 
-            //convert JSON to Region
-            Region region = gson.fromJson(reader, Region.class);
-            System.out.println(region.toString());
+    public static void readFile(String file) throws IOException {
+
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String st = br.readLine();
+        String jsonFile = "";
+        while (st != null) {
+            jsonFile += st;
+            st = br.readLine();
         }
+
+        Type listRegion = new TypeToken<ArrayList<Region>>(){}.getType();
+        ArrayList<Region> regions = gson.fromJson(jsonFile, listRegion);
+
+        for(Region region : regions) {
+            System.out.println(region);
+        }
+
     }
 
-    public static void writeFile(String file){
+    public static void writeFile(List<Region> region, String file){
+        try(Writer writer = new FileWriter(file)){
 
+            //convert Object to JSON
+            gson.toJson(region, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
