c8eec36666d3e6994b57fca88e28b220143b3da2
worker <pestekhin70@yandex.ru>
2021-03-10 17:45:29 +0700

    region sql

--- src/main/java/com/tunix70/javaio/Runner.java
+++ src/main/java/com/tunix70/javaio/Runner.java
@@ -1,5 +1,7 @@
 package com.tunix70.javaio;
 
+import com.tunix70.javaio.model.Region;
+import com.tunix70.javaio.repository.sqlRepo.RegionSQL;
 import com.tunix70.javaio.util.ConnectUtil;
 
 import java.sql.Connection;
@@ -13,21 +15,8 @@ public class Runner {
 //        ConsoleView consoleView = new ConsoleView();
 //        consoleView.startApp();
 
-        Connection connection = ConnectUtil.getInstance().getConnection();
-        Statement statement = connection.createStatement();
-        String SQL = "SELECT * FROM region";
-//        statement.execute(SQL);
-        ResultSet resultSet = statement.executeQuery(SQL);
-
-        while (resultSet.next()) {
-            int id = resultSet.getInt("id");
-            String name = resultSet.getString("name");
-
-            System.out.println("id: " + id);
-            System.out.println("Name: " + name);
-            System.out.println("\n===================\n");
-        }
-
+        RegionSQL rs = new RegionSQL();
+        rs.update(new Region(3l, "eu"));
 
     }
 }
--- src/main/java/com/tunix70/javaio/repository/sqlRepo/RegionSQL.java
+++ src/main/java/com/tunix70/javaio/repository/sqlRepo/RegionSQL.java
@@ -2,32 +2,86 @@ package com.tunix70.javaio.repository.sqlRepo;
 
 import com.tunix70.javaio.model.Region;
 import com.tunix70.javaio.repository.RegionRepository;
+import com.tunix70.javaio.util.ConnectUtil;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class RegionSQL implements RegionRepository {
+    private Connection connection = ConnectUtil.getInstance().getConnection();
+    private Statement statement;
+
     @Override
     public List<Region> getAll() {
-        return null;
+        return sqlReader();
     }
 
     @Override
-    public Region getById(Long aLong) {
-        return null;
+    public Region getById(Long id) {
+        return getAll().stream()
+                .filter(p -> id.equals(p.getId()))
+                .findFirst()
+                .orElse(null);
     }
 
     @Override
     public Region save(Region region) {
-        return null;
+        sqlWriter(region);
+        return region;
     }
 
     @Override
     public Region update(Region region) {
-        return null;
+        String SQLUpdate = "UPDATE region SET name = " + region.getName() + " WHERE id = " + region.getId();
+        try{
+            statement = connection.createStatement();
+            statement.execute(SQLUpdate);
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return region;
     }
 
     @Override
-    public void deleteById(Long aLong) {
+    public void deleteById(Long id) {
+        String SQLdeleteById = "DELETE FROM region WHERE id LIKE " + id;
+        try{
+            statement = connection.createStatement();
+            statement.execute(SQLdeleteById);
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
 
+    private List<Region> sqlReader(){
+        String SQLread = "SELECT * FROM region";
+        List<Region> regionList = new ArrayList<>();
+        try {
+            statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(SQLread);
+            while(resultSet.next()){
+                Region region = new Region();
+                region.setId((long) resultSet.getInt("id"));
+                region.setName(resultSet.getString("name"));
+                regionList.add(region);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return regionList;
+    }
+
+    public void sqlWriter(Region region){
+        String SQLadd = "INSERT INTO region (name) VALUES ('" + region.getName() + "')";
+        try{
+            statement = connection.createStatement();
+            statement.executeUpdate(SQLadd);
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 }
