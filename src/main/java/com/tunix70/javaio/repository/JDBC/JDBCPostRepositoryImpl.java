e0ed13658b83a9c2ba63407a06ea3c60df7f2b43
worker <pestekhin70@yandex.ru>
2021-03-11 17:41:14 +0700

    post edit

--- src/main/java/com/tunix70/javaio/repository/JDBC/JDBCPostRepositoryImpl.java
+++ src/main/java/com/tunix70/javaio/repository/JDBC/JDBCPostRepositoryImpl.java
@@ -14,72 +14,85 @@ import java.util.Date;
 import java.util.List;
 
 public class JDBCPostRepositoryImpl implements PostRepository {
+    private final String SQLUpdate = "UPDATE post SET name = '%s',  WHERE id = %d";
+    private final String SQLdeleteById = "DELETE FROM post WHERE id LIKE %d";
+    private final String SQLread = "SELECT * FROM post";
+    private final String SQLadd = "INSERT INTO post (name) VALUES ('%s')";
+
     private Connection connection = ConnectUtil.getInstance().getConnection();
-    private Statement statement;
-    private Long dataTime = new Date().getTime();
 
     @Override
-    public List<Post> getAll() {
-        return null;
+    public List<Region> getAll() {
+        return sqlReader();
     }
 
     @Override
-    public Post getById(Long aLong) {
-        return null;
+    public Post getById(Long id) {
+        return getAll().stream()
+                .filter(post -> id.equals(post.getId()))
+                .findFirst()
+                .orElse(null);
     }
 
     @Override
     public Post save(Post post) {
-        return null;
+        sqlWriter(post);
+        return post;
     }
 
     @Override
     public Post update(Post post) {
-        return null;
+        try (Statement statement = connection.createStatement()){
+            statement.execute(String.format(SQLUpdate, post.getName(), post.getId()));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return post;
     }
 
     @Override
-    public void deleteById(Long aLong) {
-
+    public void deleteById(Long id) {
+        try (Statement statement = connection.createStatement()){
+            statement.execute(String.format(SQLdeleteById, id));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
-    private List<Post> sqlReader(){
-        String SQLread = "SELECT * FROM post";
-        List<Post> postList = new ArrayList<>();
-        try {
-            statement = connection.createStatement();
+    public List<Post> sqlReader() {
+        List<Post> listpost= new ArrayList<>();
+        try (Statement statement = connection.createStatement()){
             ResultSet resultSet = statement.executeQuery(SQLread);
-            while(resultSet.next()){
-                Post post = new Post();
-                post.setId((long) resultSet.getInt("id"));
-                post.setContent(resultSet.getString("content"));
-                postList.add(post);
-            }
+            listpost = getPostFromSQL(resultSet);
         } catch (SQLException throwables) {
             throwables.printStackTrace();
-        }finally {
-            try {
-                statement.close();
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
+        }
+        return listpost;
+    }
+
+    public void sqlWriter(Post post) {
+        try (Statement statement = connection.createStatement()){
+            statement.executeUpdate(String.format(SQLadd, post.getContent()));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Post> getPostFromSQL(ResultSet resultSet) {
+        List<Post> postList = new ArrayList<>();
+        try {
+            while (resultSet.next()) {
+                Post post = new Post();
+                post.setId((long) resultSet.getInt("id"));
+                post.setContent(resultSet.getString("name"));
+                post.setCreated(resultSet.getTimestamp("created"));
+                post.setCreated(resultSet.getTimestamp("updated"));
+                post.setPostStatus(resultSet.getString("postStatusID"));
+                postList.add(post);
             }
+        }catch (SQLException throwables) {
+            throwables.printStackTrace();
         }
         return postList;
     }
-
-//    public void sqlWriter(Post post){
-//        String SQLadd = "INSERT INTO post (content, created, updated) VALUES ('" + post.getContent() + "', '" + post.getCreated() + "'"")";
-//        try{
-//            statement = connection.createStatement();
-//            statement.executeUpdate(SQLadd);
-//        }catch (SQLException e){
-//            e.printStackTrace();
-//        }finally {
-//            try {
-//                statement.close();
-//            } catch (SQLException throwables) {
-//                throwables.printStackTrace();
-//            }
-//        }
-//    }
 }
