e22bdc3cfe8036f881703c856bad314d64153986
homer <pestekhin70@yandex.ru>
2021-03-24 23:17:16 +0700

    first test mock

--- src/test/java/com/tunix70/javaio/service/RegionServiceTest.java
+++ src/test/java/com/tunix70/javaio/service/RegionServiceTest.java
@@ -2,34 +2,35 @@ package com.tunix70.javaio.service;
 
 
 import com.tunix70.javaio.model.Region;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 
 class RegionServiceTest {
-    private static Region region1;
-    private static Region region2;
 
-    @BeforeClass
-    public static void setUp() {
-        //create mock object of RegionService
-        RegionService regionService = mock(RegionService.class);
-
-        //create new instances of Region class
-        region1 = new Region(1L, "RU");
-        region2 = new Region(2L, "UA");
-
-        when(regionService.getAll()).thenReturn(Arrays.asList(region1, region2));
-        when(regionService.getById(1l)).thenReturn(region1);
-
-    }
     @Test
     void test_getAll_region() {
+        RegionService regionServiceMock = mock(RegionService.class);
+        Region region1 = new Region(1l, "ru");
+        Region region2 = new Region(2l, "eu");
+        List<Region> regionList = new ArrayList<>(Arrays.asList(region1, region2));
+        when(regionServiceMock.getAll()).thenReturn(regionList);
 
+        List<Region> regionList1 = regionServiceMock.getAll();
+        Region region = regionList1.get(0);
+        assertEquals(2, regionList.size());
+        assertEquals("ru", region.getName());
     }
 
     @Test
